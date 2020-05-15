f8260505c681effcf3d4c3d92b3475eefc4ffd68
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-05-15 09:29:26 -0700

    Add test to return an empty array when there are no words and fail test

--- spec/word_definer_spec.rb
+++ spec/word_definer_spec.rb
@@ -1,3 +1,11 @@
 require 'rspec'
 require 'word_definer'
 require 'pry'
+
+describe ('#Word') do
+  describe ('.all') do
+    it('returns am empty array when there are no words') do
+      expect(Word.all).to(eq([]))
+    end
+  end
+end
