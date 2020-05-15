bec40276e489513e8be258b97bcd87c1f8c5ba9d
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-05-15 11:07:37 -0700

    Add a home page

--- app.rb
+++ app.rb
@@ -3,3 +3,13 @@ require('sinatra/reloader')
 require('./lib/word_definer')
 require('pry')
 also_reload('lib/**/*.rb')
+
+get('/') do
+  @texts = Word.all
+  erb(:home)
+end
+
+get('/words') do
+  @texts = Word.all
+  erb(:home)
+end
