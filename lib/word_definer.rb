5949f52b52143c4a6a22cbde87e290de0c920637
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-05-15 09:35:26 -0700

    Add method and pass test

--- lib/word_definer.rb
+++ lib/word_definer.rb
@@ -1,1 +1,19 @@
 require 'pry'
+
+class Word
+  attr_reader :id
+  attr_accessor :text
+
+  @@words = {}
+  @total_words = 0
+
+  def initialize(text, id)
+    @text = text
+    @id = id || total_words += 1
+  end
+
+  def self.all
+    @@words.values()
+  end
+  
+end
