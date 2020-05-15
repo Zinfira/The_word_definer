018d21fa463bc8c941e9f439c17f72014fcb2883
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-05-15 11:29:23 -0700

    Add a view and functionality to create new word

--- app.rb
+++ app.rb
@@ -13,3 +13,15 @@ get('/words') do
   @texts = Word.all
   erb(:home)
 end
+
+get('/words/new') do
+  erb(:new_word)
+end
+
+post('/words') do
+  new_word = params[:text]
+  text = Word.new({:text => new_word})
+  text.save()
+  @texts = Word.all()
+  erb(:home)
+end
